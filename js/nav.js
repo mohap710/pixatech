14b2f255d7acebd1f36614bf6f4e8e98866c9b92
mohap710 <Mohap710@gmail.com>
2024-06-02 23:23:25 +0400

    Init commit

---
+++ js/nav.js
@@ -0,0 +1,34 @@
+let navToggler = document.querySelector(".nav-toggler");
+let nav = document.querySelector("#main-nav");
+let header = document.querySelector("header");
+
+function openNav() {
+	navToggler.setAttribute("aria-expanded", "true");
+	navToggler.classList.add("opened");
+	nav.setAttribute("data-visible", "true");
+	// prevent body from scrolling when menu is opened
+	// document.querySelector("body").style.overflow = "hidden";
+}
+function closeNav() {
+	navToggler.setAttribute("aria-expanded", "false");
+	navToggler.classList.remove("opened");
+	nav.setAttribute("data-visible", "false");
+	// document.querySelector("body").style.overflow = "scroll";
+}
+function toggleNav() {
+	const visibility = nav.getAttribute("data-visible");
+	visibility === "false" ? openNav() : closeNav();
+
+}
+// Change between transparent and colored nav
+window.addEventListener("scroll", () => {
+	window.scrollY > 25
+		? header.classList.add("colored-nav")
+		: header.classList.remove("colored-nav");
+});
+// Change active state and close nav if opened
+function setActiveClass(index) {
+	activeLink.value = index;
+	const visibility = nav.value.getAttribute("data-visible");
+	if (visibility === "true") closeNav();
+}
